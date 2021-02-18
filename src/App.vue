acc3d779fa2ee13a3c1363158653d4c0b4ed4f23
CodeStarting-design <1225499794@qq.com>
2021-02-18 22:02:58 +0800

    ‘项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Upload/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
+import Upload from './components/upload.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Upload
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import ElementUI from 'element-ui'; // 导入element-ui包
+import 'element-ui/lib/theme-chalk/index.css'; // 导入相关样式
+Vue.use(ElementUI); // 引用
 
 Vue.config.productionTip = false
 
